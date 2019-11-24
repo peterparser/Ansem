69fd5f90c34a8a7d150f5411ff361e97dcce17b5
max <massimo.schembri18@gmail.com>
2019-11-24 14:29:37 +0100

    Fix print

--- internal/team.go
+++ internal/team.go
@@ -10,7 +10,7 @@ func GetTeamAsChan(fileTeam string) chan string {
 	teamChannel := make(chan string, 20)
 	file, err := os.Open(fileTeam)
 	if err != nil {
-		log.Fatalf("TEAM\t Error Team File:\t  %s \n", fileTeam)
+		log.Fatalf("TEAM:\t%s is not a valid file!\n", fileTeam)
 	}
 
 	var lines []string
